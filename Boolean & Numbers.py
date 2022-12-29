959efa21d3eb93c2ce288fef915aaf437373b37c
NIKHIL YADAV <114735514+nikhilyadav7782@users.noreply.github.com>
2022-12-29 15:27:15 +0530

    Add files via upload

---
+++ Boolean & Numbers.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+x=5.0
+
+
+# In[13]:
+
+
+type(x)
+
+
+# In[17]:
+
+
+int(x)
+
+
+# In[27]:
+
+
+x=3
+
+
+# In[28]:
+
+
+type(x)
+
+
+# In[29]:
+
+
+float(x)
+
+
+# In[21]:
+
+
+x=True
+
+
+# In[22]:
+
+
+type(x)
+
+
+# In[23]:
+
+
+x=true
+
+
+# In[ ]:
+
+
+
+
